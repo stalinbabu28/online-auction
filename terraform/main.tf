2c8146507d59cb9bf70a22843fd6c41efa5a6885
Stalin Babu Nakka <stalinbabu.nakka@gmail.com>
2025-03-16 09:14:45 +0530

    feat: add Azure Container Registry and update AKS configuration in Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,30 +16,40 @@ resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
   prefix = "dns"
 }
 
-resource "azurerm_kubernetes_cluster" "k8s" {
-  location            = azurerm_resource_group.rg.location
-  name                = random_pet.azurerm_kubernetes_cluster_name.id
+resource "azurerm_container_registry" "acr" {
+  name                = "auctionifyacr"
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = "auctionifyaks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "auctionifyaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+  }
 
   identity {
     type = "SystemAssigned"
   }
 
-  default_node_pool {
-    name       = "agentpool"
-    vm_size    = "Standard_D2_v2"
-    node_count = var.node_count
-  }
-  linux_profile {
-    admin_username = var.username
-
-    ssh_key {
-      key_data = azapi_resource_action.ssh_public_key_gen.output.publicKey
-    }
-  }
   network_profile {
-    network_plugin    = "kubenet"
-    load_balancer_sku = "standard"
+    network_plugin = "azure"
+    network_policy = "azure"
   }
+
+  depends_on = [azurerm_container_registry.acr]
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,17 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "kubernetes_cluster_name" {
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.k8s.name
 }
 
+output "aks_cluster_kube_config" {
+  value = azurerm_kubernetes_cluster.k8s.kube_config_raw
+}
 output "client_certificate" {
   value     = azurerm_kubernetes_cluster.k8s.kube_config[0].client_certificate
   sensitive = true
