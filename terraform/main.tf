c2ab14723febc0b7f7e492fc271048a5402c9e63
Stalin Babu Nakka <stalinbabu.nakka@gmail.com>
2025-03-16 15:14:01 +0530

    fix: disable HTTP application routing in Azure Kubernetes cluster configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,6 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "auctionifyaks"
-  http_application_routing_enabled = "true"
 
   default_node_pool {
     name       = "default"
